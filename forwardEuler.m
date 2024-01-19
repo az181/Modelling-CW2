b4cf28670fb0afe278ccda49dbd91eb0d0226dd7
Atticus Hennessy-Vass <atticushv@gmail.com>
2024-01-19 12:17:06 +0000

    implementend the forward Euler functinon

--- forwardEuler.m
+++ forwardEuler.m
@@ -1,4 +1,11 @@
-function y = forwardEuler(x, f, f')
-%forwardEuler - forward Euler on f with input of x
-    
+function y = forwardEuler(hight, f, h)
+%forwardEuler - forward Euler on f where f is a function with input y and t (in that ordder )
+t = [0];
+y = [hight];
+n = 1;
+    while y(n) > 0:
+        y(n+1) = y(n) + h*f(y(n),t(n));
+        t(n+1) = t(n) + h;
+        n = n + 1;
+    end
 end
