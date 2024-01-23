70df84ff4fff1cd9f67beda07eeee104c8c7a6e0
Atticus Hennessy-Vass <atticushv@gmail.com>
2024-01-23 10:43:13 +0000

    fixed typos

--- forwardEuler.m
+++ forwardEuler.m
@@ -1,11 +1,11 @@
-function y = forwardEuler(hight, f, h)
-%forwardEuler - forward Euler on f where f is a function with input y and t (in that ordder )
+function y = forwardEuler(height, f, h)
+% forwardEuler - forward Euler on f where f is a function with input y and t (in that ordder )
 t(1) = 0;
-y(1) = hight;
+y(1) = height;
 n = 1;
 while y(n) > 0
     y(n + 1) = y(n) + h * f(y(n), t(n));
     t(n + 1) = t(n) + h;
     n = n + 1;
 end
-end
+end 
