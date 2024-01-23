064d46d8ca329620cf38fb27b2bc0a3b910a57f6
Atticus Hennessy-Vass <atticushv@gmail.com>
2024-01-23 11:19:56 +0000

    added a functoin handle

--- forwardEuler.m
+++ forwardEuler.m
@@ -1,10 +1,10 @@
-function y = forwardEuler(height, f, h)
+function y = forwardEuler(height, @dydt, h)
 %forwardEuler - forward Euler on f where f is a function with input y and t (in that ordder )
 t(1) = 0;
 y(1) = height;
 n = 1;
 while y(n) > 0
-    y(n + 1) = y(n) + h * f(y(n), t(n));
+    y(n + 1) = y(n) + h * dydt(y(n), t(n));
     t(n + 1) = t(n) + h;
     n = n + 1;
 end
