206af52a9bae65c346d9302aea307efb260e660a
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-01-23 11:24:46 +0000

    Update trajectory_eq.m
     change trajectory to a function of t

--- trajectory_eq.m
+++ trajectory_eq.m
@@ -1,4 +1,4 @@
-function [y] = trajectory_eq(x, alpha, v)
+function [y] = trajectory_eq(t, alpha, v)
 g = -9.81;
 %calcuate the trajectory y of the particle at point x when fired at and
 %angle alpha to the horizontal at initial speed v.
