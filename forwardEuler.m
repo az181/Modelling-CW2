5264ae7d49084b7fb11d4d67b04a0d846856313b
Atticus Hennessy-Vass <atticushv@gmail.com>
2024-01-24 14:44:05 +0000

    added v and alpha to forwardEuler

--- forwardEuler.m
+++ forwardEuler.m
@@ -1,10 +1,10 @@
-function y = forwardEuler(height, dydt, h)
+function y = forwardEuler(height, dydt, h, v, alpha)
 %forwardEuler - forward Euler on f where f is a function with input y and t (in that ordder )
 t(1) = 0;
 y(1) = height;
 n = 1;
 while y(n) > 0
-    y(n + 1) = y(n) + h * dydt(y(n), t(n));
+    y(n + 1) = y(n) + h * dydt(y(n), t(n), v, alpha);
     t(n + 1) = t(n) + h;
     n = n + 1;
 end
