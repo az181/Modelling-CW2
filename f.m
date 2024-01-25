c9619c5e05418973308ba37bed3db1954c00d4e8
KyleProfit <kp598@exeter.ac.uk>
2024-01-25 11:19:19 +0000

    created function for plotting the path

--- f.m
+++
@@ -1,6 +0,0 @@
-function dy = f(y, t)
-%function to find the gradient of the trajectory for use in the Forward 
-%Euler scheme
-g = -9.81;
-dy = g * t;
-end
--- forwardEuler.m
+++ forwardEuler.m
@@ -1,11 +1,15 @@
-function y = forwardEuler(height, dydt, h, v, alpha)
+function [x, y] = forwardEuler(H_ball, dydt, h, v, alpha)
 %forwardEuler - forward Euler on f where f is a function with input y and t (in that ordder )
-t(1) = 0;
-y(1) = height;
+t(1) = 0
+y(1) = H_ball
+%output is also a array of x values used when plotting the path
+x(1) = 0
 n = 1;
 while y(n) > 0
     y(n + 1) = y(n) + h * dydt(y(n), t(n), v, alpha);
     t(n + 1) = t(n) + h;
+    %x = velocity * time
+    x(n + 1) = v * cos(alpha) * t(n + 1);
     n = n + 1;
 end
 end
