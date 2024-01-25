349103a64b55ca6a19fd813a077d05b6bdb56b40
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-01-25 18:48:00 +0000

    Update trajectory_eq.m
    made z y for consistency

--- trajectory_eq.m
+++ trajectory_eq.m
@@ -1,7 +1,7 @@
-function [x,z] = trajectory_eq(t, alpha, v)
+function [x,y] = trajectory_eq(t, alpha, v)
 load("constants.mat");
 %calcuate the trajectory y of the particle at point x when fired at and
 %angle alpha to the horizontal at initial speed v.
 x = v * t * cos(alpha);
-z = (v * t * sin(alpha)) - ( (-g * t ^ 2) / 2);
+y = (v * t * sin(alpha)) - ( ((-g) * t ^ 2) / 2);
 end
