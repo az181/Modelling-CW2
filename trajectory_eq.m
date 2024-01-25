b61369fb0ad17f66a2d1479f18fe2f6fbe561a66
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-01-25 12:28:06 +0000

    Update trajectory_eq.m
    added load(constants)
    made gravity negative

--- trajectory_eq.m
+++ trajectory_eq.m
@@ -1,7 +1,7 @@
-function [y] = trajectory_eq(t, alpha, v)
-g = -9.81;
+function [x,z] = trajectory_eq(t, alpha, v)
+load("constants.mat");
 %calcuate the trajectory y of the particle at point x when fired at and
 %angle alpha to the horizontal at initial speed v.
 x = v * t * cos(alpha);
-z = v * t * sin(alpha) - (g * t ^ 2) / 2;
+z = (v * t * sin(alpha)) - ( (-g * t ^ 2) / 2);
 end
