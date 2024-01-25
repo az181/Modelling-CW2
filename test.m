c274ed9f489b710aab2938460acee4bfc0c8e058
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-01-25 19:56:01 +0000

    Ahhhhhhhhh
    Made Script to plot "actual" motion of the ball

---
+++ test.m
@@ -0,0 +1,21 @@
+%declare stuff
+load("constants.mat")
+x = [];
+y = [];
+x(1) = 0;
+y(1) = H_ball;
+t=0;
+n=1;
+%variables to change (yeah I know this needs to be a function)
+h=0.05;
+alpha = pi/4;
+v = 2;
+%generate values
+while y(n) > 0 & n < 150
+    t = t+h;
+    [x(n+1),y(n+1)] = trajectory_eq(t,alpha,v);
+    y(n+1) = y(n+1) + H_ball
+    n=n+1;
+end
+
+plot(x,y)
--- trajectory_eq.m
+++ trajectory_eq.m
@@ -3,5 +3,5 @@ load("constants.mat");
 %calcuate the trajectory y of the particle at point x when fired at and
 %angle alpha to the horizontal at initial speed v.
 x = v * t * cos(alpha);
-y = (v * t * sin(alpha)) - ( ((-g) * t ^ 2) / 2);
+y = (v * t * sin(alpha)) - ( ((g) * (t ^ 2)) / 2);
 end
