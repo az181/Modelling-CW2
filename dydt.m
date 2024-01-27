d54bcd1df91981e3abad0e42da75f9a2827d7a40
Atticus Hennessy-Vass <atticushv@gmail.com>
2024-01-27 21:35:10 +0000

    added ; s

--- dydt.m
+++ dydt.m
@@ -1,6 +1,8 @@
-function dy = dydt(y, t, v, alpha)
+function dv = dydt(y, t, v, alpha)
 %function to find the gradient of the trajectory for use in the Forward 
 %Euler scheme
 g = -9.81;
 dy = v * sin(alpha) + g * t;
+dx = v * cos(alpha) * t;
+dv = [dx, dy];
 end
--- trajctory_test.m
+++ trajctory_test.m
@@ -1,13 +1,13 @@
-h = 0.1
-x(1) = 0
-y(1) = 0.8
+h = 0.1;
+x(1) = 0;
+y(1) = 0.8;
 t = 0;
 n = 1;
 while y(n) >= 0
-    n = n + 1
-    t = t + h
-    [x(n), y(n)] = trajectory_eq(t, 12, 1)
-    y(n) = y(n) + 0.8
+    n = n + 1;
+    t = t + h;
+    [x(n), y(n)] = trajectory_eq(t, 12, 1);
+    y(n) = y(n) + 0.8;
 end
 
 plot(x, y)
