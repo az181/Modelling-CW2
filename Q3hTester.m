f10daec97fe081e8a469484028cab3618762c6a9
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-01-31 14:52:49 +0000

    made outline of code for q3
    This is the start of my answer to Q3) 2) in which we need to show how varying h changes the error. This should be a general code as we have to compare two different time stepping methods.

---
+++ Q3hTester.m
@@ -0,0 +1,26 @@
+%test for various h
+%win
+
+load("constants.mat")
+
+%should be inputs
+v = 2;
+alpha = pi/3;
+bigT = %a number
+
+%make h yo
+h = 1:1:100;
+n = 1;
+theRealValue = trajectory_eq(bigT,v,alpha);
+
+% check lots of different h and see the error to our "true" value
+while n<100
+    error = abs(forwardEuler(H_ball,dxdt,dydt,h,v,alpha) - theRealValue);
+    n = n+1;
+end
+%might want to plot too
+%also i think this should be a function with the appropriate variables as
+%inputs
+
+
+%hopefully my madness makes some sense here
