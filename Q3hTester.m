8c674c9f1d926a3278c191d50e4e6abefdb2f324
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-02-04 13:46:26 +0000

    made trajectory with vector output

--- Q3hTester.m
+++ Q3hTester.m
@@ -11,12 +11,12 @@ bigT = 55; %should find out what t is when the ball hits the ground
 %make things that are used just for this script
 h = 1:1:100;
 n = 1;
-theRealValue = trajectory_eq(bigT,v,alpha);
+theRealValue = trajectory_eq_vector(bigT,v,alpha);
 error = zeros(1,100); %pre allocating array makes it faster by an infeasible amount
 
 % check lots of different h and see the error to our "true" value
 while n<100
-    error(n) = abs(forwardEuler(H_ball,dxdt,dydt,h,v,alpha) - theRealValue); %the function here should be an input
+    error(n) = abs(forwardEuler(H_ball,@dxdt,@dydt,h,v,alpha) - theRealValue); %the function here should be an input
     n = n+1;
 end
 %might want to plot too
--- trajectory_eq.m
+++ trajectory_eq.m
@@ -1,7 +1,8 @@
-function [x,y] = trajectory_eq(t, v, alpha)
+function [x,y,pos] = trajectory_eq(t, v, alpha)
 load("constants.mat");
 %calcuate the trajectory y of the particle at point x when fired at and
 %angle alpha to the horizontal at initial speed v.
 x = v * t * cos(alpha);
 y = (v * t * sin(alpha)) - ( ((g) * (t ^ 2)) / 2);
+
 end
