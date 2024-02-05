bd5784def90e5bc159356db0716740b1377d8c16
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-02-05 17:36:19 +0000

    Update midpointMethod.m

--- midpointMethod.m
+++ midpointMethod.m
@@ -1,15 +1,15 @@
-H_ball = 0.8;
-T = 100;%whatever you want
-h = 0.1;%or whatever you want
+%H_ball = 0.8;
+%T = 100;%whatever you want
+%h = 0.1;%or whatever you want
 %i hop this is what i was supposed to do
 %if it is wrong i am sorry and tell me where I'm wrong
-function [x, y] = MidpointMethod(T, H_ball, h, trajectory_eq)
+function [x, y] = midpointMethod(T, H_ball, h, trajectory_eq)
     t = 0:h:T; % time array
     x = zeros(size(t));
     y = zeros(size(t));
     y(1) = H_ball; % y starts at the height of the ball
 
-    for i = 1:length(t)-1
+    for i = 1:(length(t)-1)
         mid = t(i) + h / 2; % finds midpoint
         [xMid, yMid] = trajectory_eq(mid, x(i), y(i));
         %calculate slopes at midpoint using trajectory_eq
