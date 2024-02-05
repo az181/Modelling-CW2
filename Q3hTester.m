6c0c95e7c4f116fbc1456cca12b636ead33b5d59
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-02-05 12:41:14 +0000

    Major changes to Q3hTester
    also added gitignore to ignore autosave files

--- Q3hTester.m
+++ Q3hTester.m
@@ -9,18 +9,30 @@ alpha = pi/10;
 bigT = 55; %should find out what t is when the ball hits the ground
 
 %make things that are used just for this script
-h = 1:1:100;
+h = 0.01:0.01:1;
 n = 1;
-theRealValue = trajectory_eq_vector(bigT,v,alpha);
+pos = trajectory_eq(bigT,v,alpha);
+theRealValue = pos;
 error = zeros(1,100); %pre allocating array makes it faster by an infeasible amount
+
 
 % check lots of different h and see the error to our "true" value
 while n<100
-    error(n) = abs(forwardEuler(H_ball,@dxdt,@dydt,h,v,alpha) - theRealValue); %the function here should be an input
+    Val = forwardEuler(H_ball,@dxdt,@dydt,h(n),v,alpha);
+    error(n) = abs(Val(length(Val)) - theRealValue); %the function here should be an input
     n = n+1;
 end
+%drop final values because my indexing doesn't work
+h(end) = [];
+error(end) = [];
+
 %might want to plot too
-%also i think this should be a function with the appropriate variables as
-%inputs
+figure(1)
+plot(h, error)
+xlabel("Value for h")
+ylabel("Error when compared to trajectory equation")
+title("Error of Forward Euler as h varies")
+%set the axis limits to positive only
+ylim([0, inf])
 
 %hopefully my madness makes some sense here
