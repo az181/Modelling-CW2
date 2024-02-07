1de6a7c5dc9b2b1c91944c039ecbfb62d2f4c4dd
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2024-02-07 10:01:18 +0000

    Update q3midpoint.m

--- q3midpoint.m
+++ q3midpoint.m
@@ -18,7 +18,7 @@ for n = 1:length(h)
     [mpx,mpy] = midpointMethod(bigT, H_ball, h(n), @trajectory_eq, v, alpha);
     % disp(mpx);
     xerror(n) = (mpx(end) - xtr);
-    yerror(n) = (mpy(end) - ytr);
+    yerror(n) = (mpy(end) - ytr); %floor(abs(bigT/h(n)))
     error(n) = sqrt((xerror(n))^2 + (yerror(n))^2);
 end
 % plot
@@ -26,6 +26,6 @@ figure(1)
 loglog(h, error)
 xlabel("Value for h")
 ylabel("Error when compared to trajectory equation")
-title("Error of Forward Euler as h varies")
+title("Error of Midpoint Method as h varies")
 % set the axis limits to positive only
 ylim([0, inf])
